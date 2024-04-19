b2b3322f1f12cad0a8cd359b780f352e95fb7e15
Gabriele <gabriele.palma.bool@gmail.com>
2024-04-19 16:23:26 +0200

    add

--- Program.cs
+++ Program.cs
@@ -29,13 +29,161 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace csharp_oop_shop
 {
+
+    //classe prodotto
+    public class Prodotto
+    {
+        //variabile codice privata
+        private int codice;
+
+        //Dichiaro un metodo per la sola lettura della variabile codice (properties)
+        public int Codice => codice;
+
+        //Funzione per la lettura e scrittura della variabile (properties)
+        public string Nome { get; set; }
+
+        //Funzione per la lettura e scrittura della variabile (properties)
+        public string Descrizione { get; set; }
+
+        //Funzione per la lettura e scrittura della variabile (properties)
+        public decimal Prezzo { get; set; }
+
+        //Funzione per la lettura e scrittura della variabile (properties)
+        public decimal Iva { get; set; }
+
+        //costruttore
+        public Prodotto(string nome, string descrizione, decimal prezzo, decimal iva) 
+        {
+            this.Nome = nome;
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
+            //assegno alla varaibile codice l'intero passato dal metodo privato GeneraCodice()
+            this.codice = GeneraCodice();
+        }
+
+        //METODI
+
+        private int GeneraCodice()
+        {
+            Random rnd = new Random();
+            return rnd.Next(10000000); // Numero casuale a 8 cifre
+        }
+
+        //metodo che restituisce il prezzo base in decimale
+        public decimal PrezzoBase()
+        {
+            return this.Prezzo;
+        }
+
+        //metodo che restituisce il prezzo base + IVA in decimale
+        public decimal PrezzoConIVA()
+        {
+            return this.Prezzo + (this.Prezzo * this.Iva / 100);
+        }
+
+        //metodo che restituisce il codice + nome
+        public string NomeEsteso()
+        {
+            return this.Codice.ToString("D8") + this.Nome;
+        }
+
+        public string CodiceFormat()
+        {
+            return this.Codice.ToString("D8");
+        }
+
+
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            //per l'icona euro su console
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            // Creazione di un prodotto
+            Prodotto prodotto1 = new Prodotto("Penne", "Scatola di penne nere", 2.5m, 22);
+
+            // Test dei metodi
+            Console.WriteLine("Codice: " + prodotto1.CodiceFormat());
+            Console.WriteLine("Nome: " + prodotto1.Nome);
+            Console.WriteLine("Descrizione: " + prodotto1.Descrizione);
+            Console.WriteLine("Prezzo base: " + prodotto1.PrezzoBase() + " \u20AC");
+            Console.WriteLine("Prezzo con IVA: " + prodotto1.PrezzoConIVA() + " \u20AC");
+            Console.WriteLine("Nome esteso: " + prodotto1.NomeEsteso());
+
+
+            Console.WriteLine("\n\n****CREAZIONE SINGOLI PRODOTTI*****");
+
+            int TotProd;
+
+            Console.WriteLine("\nQuanti prodotti vuoi inserire?:");
+            //controllo sull'input dell'utente, se quello che è stato digitato non è un numero darà errore
+            while (int.TryParse(Console.ReadLine(), out TotProd) == false)
+            {
+                Console.WriteLine("Sintassi errata. Inserisci numero");
+            }
+
+
+            string nome, descrizione;
+
+            decimal prezzo, iva;
+
+            //creo un array di prodotti
+            Prodotto[] listaProdotti = Population(TotProd);
+
+            for (int i= 0; i< listaProdotti.Length; i++)
+            {
+                Console.WriteLine($"\nInserisci nome {i+1} prodotto:");
+                nome = Console.ReadLine();
+
+                Console.WriteLine($"\nInserisci descrizione {i+1} prodotto:");
+                descrizione = Console.ReadLine();
+
+                Console.WriteLine($"\nInserisci il prezzo del {i + 1} prodotto:");
+                //controllo sull'input dell'utente, se quello che è stato digitato non è un numero darà errore
+                while (decimal.TryParse(Console.ReadLine(), out prezzo) == false)
+                {
+                    Console.WriteLine("Sintassi errata. Inserisci numero");
+                }
+
+                Console.WriteLine($"\nInserisci l'iva del {i + 1} prodotto:");
+                //controllo sull'input dell'utente, se quello che è stato digitato non è un numero darà errore
+                while (decimal.TryParse(Console.ReadLine(), out iva) == false)
+                {
+                    Console.WriteLine("Sintassi errata. Inserisci numero");
+                }
+
+                //inserisco alla posizione i del mio array l'oggetto prodotto con tutti i valori richiesti
+                listaProdotti[i] = new Prodotto(nome, descrizione, prezzo, iva);
+            }
+
+            Console.WriteLine("\n\nLa lista dei prodotti sarà:");
+            for (int i = 0; i < listaProdotti.Length; i++)
+            {
+                // Test dei metodi
+                Console.WriteLine($"\nPosizione: {i+1}");
+                Console.WriteLine("Codice: " + listaProdotti[i].CodiceFormat());
+                Console.WriteLine("Nome: " + listaProdotti[i].Nome);
+                Console.WriteLine("Descrizione: " + listaProdotti[i].Descrizione);
+                Console.WriteLine("Prezzo base: " + listaProdotti[i].PrezzoBase() + " \u20AC");
+                Console.WriteLine("Prezzo con IVA: " + listaProdotti[i].PrezzoConIVA() + " \u20AC");
+                Console.WriteLine("Nome esteso: " + listaProdotti[i].NomeEsteso());
+
+            }
+
+
+            //funzione
+            Prodotto[] Population( int number)
+            {
+                return new Prodotto[number];
+            }
 
             Console.ReadKey();
         }
